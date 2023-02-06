0b34adfdecf56018e2914a5c279fb1b488466287
Diana Raharinosy <dianaraharinosy@yahoo.com>
2023-02-06 15:22:25 +0100

    create read and update done

--- src/Controller/AdminCategoryController.php
+++ src/Controller/AdminCategoryController.php
@@ -16,10 +16,11 @@ class AdminCategoryController extends AbstractController
     #[Route('/admin/category', name:'app_admin_category')]
     public function index(): Response
     {
-        return $this->render('admin_category/index.html.twig', [
+        return $this->render('admin_category/index.html.twig',[
             'controller_name' => 'AdminCategoryController',
         ]);
     }
+
 
 
     #[Route("/gestion/afficher", name:"category_afficher")]
@@ -30,6 +31,7 @@ class AdminCategoryController extends AbstractController
             "categories" => $categories
         ]);
     }
+
 
     #[Route("/gestion/ajouter", name: "category_ajouter")]
     public function category_ajouter(Request $request, EntityManagerInterface $manager)
@@ -68,10 +70,11 @@ class AdminCategoryController extends AbstractController
             Si le formulaire a été soumis (l’utilisateur a cliqué sur le bouton submit) et si le formulaire est valide
             (placer des conditions/contraintes sur le formulaire, input non vide, entre xx et xx caractères etc...)
             */
-            dump($category);
+            // dump($category);
             $manager->persist($category); //définir l'objet à envoyer
             $manager->flush(); //envoyer
-            dd($category);
+            // dump($manager->flush());
+            // dd($category);
             /*
             On utilise 2 méthodes de la class EntityManagerInterface:
 
@@ -80,6 +83,9 @@ class AdminCategoryController extends AbstractController
 
             Flush() permet d’envoyer en base de données
             */
+            $this->addFlash('succes', "La catégorie N°". $category->getId()."a bien été ajoutée");
+            
+            return $this->redirectToRoute("category_afficher");
 
         }
 
@@ -89,6 +95,34 @@ class AdminCategoryController extends AbstractController
             // dans l'objet form se trouve une methode createView permettant de créer la structure en html du formulaire
         ]);
 
+
+    }
+
+   #[Route('/gestion/modifier/{id}', name: "category_modifier")]
+    public function category_modifier($id , CategoryRepository $repoCategory, Request $request , EntityManagerInterface $manager)
+    {
+        // dd($id);
+        $category = $repoCategory->find($id);
+        // dd($category);
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+
+            $manager->persist($category);
+            $manager->flush();
+
+            $this->addFlash("succes", "La catégorie N°" . $category->getId() . "a bien été modifié");
+            return $this->redirectToRoute('category_afficher');
+
+        }
+
+        return $this->render("admin_category/category_modifier.html.twig", [
+            // dans l'objet form se trouve une methode createView permettant de créer la structure en html du formulaire
+            "formCategory" => $form->createView(),
+            "category"=> $category
+        ]);
 
     }
 
